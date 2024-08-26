67588a4e0cf23b11e3486b8b37ccf9d454a58ab8
Hilliam T <hilliamtung@gmail.com>
2024-08-26 20:24:04 +0100

    fix zone addresses

--- src/constants.rs
+++ src/constants.rs
@@ -29,34 +29,12 @@ pub enum ChainId {
 
 pub fn AORI_V2_SINGLE_CHAIN_ZONE_ADDRESSES() -> HashMap<ChainId, HashSet<String>> {
     HashMap::from([
-        (
-            ChainId::Optimism,
-            HashSet::from(["0xcc1A0DA89593441571f35Dd99a0aC1856d3F1FB5".to_string()]),
-        ),
-        (
-            ChainId::Polygon,
-            HashSet::from(["0xcc1A0DA89593441571f35Dd99a0aC1856d3F1FB5".to_string()]),
-        ),
-        (ChainId::Blast, HashSet::from(["0xcc1A0DA89593441571f35Dd99a0aC1856d3F1FB5".to_string()])),
-        (ChainId::Base, HashSet::from(["0xcc1A0DA89593441571f35Dd99a0aC1856d3F1FB5".to_string()])),
-        (
-            ChainId::Arbitrum,
-            HashSet::from([
-                "0xcc1A0DA89593441571f35Dd99a0aC1856d3F1FB5".to_string(),
-                "0x6A979916234013AbA003d906e4e7136496B90AA6".to_string(),
-            ]),
-        ),
-        (
-            ChainId::Sepolia,
-            HashSet::from([
-                "0xcc1A0DA89593441571f35Dd99a0aC1856d3F1FB5".to_string(),
-                "0x6A979916234013AbA003d906e4e7136496B90AA6".to_string(),
-            ]),
-        ),
-        (
-            ChainId::BerachainArtio,
-            HashSet::from(["0xcc1A0DA89593441571f35Dd99a0aC1856d3F1FB5".to_string()]),
-        ),
+        (ChainId::Optimism, HashSet::from(["0x0AD86842EadEe5b484E31db60716EB6867B46e21".to_string()])),
+        (ChainId::Polygon, HashSet::from(["0x0AD86842EadEe5b484E31db60716EB6867B46e21".to_string()])),
+        (ChainId::Blast, HashSet::from(["0x0AD86842EadEe5b484E31db60716EB6867B46e21".to_string()])),
+        (ChainId::Base, HashSet::from(["0x0AD86842EadEe5b484E31db60716EB6867B46e21".to_string()])),
+        (ChainId::Arbitrum, HashSet::from(["0x0AD86842EadEe5b484E31db60716EB6867B46e21".to_string()])),
+        (ChainId::Sepolia, HashSet::from(["0x0AD86842EadEe5b484E31db60716EB6867B46e21".to_string()])),
     ])
 }
 
@@ -66,7 +44,7 @@ pub fn SUPPORTED_AORI_CHAINS() -> HashSet<U256> {
     )
 }
 
-pub const DEFAULT_ZONE: &str = "0xeA2b4e7F02b859305093f9F4778a19D66CA176d5";
+pub const DEFAULT_ZONE: &str = "0x0AD86842EadEe5b484E31db60716EB6867B46e21";
 pub const DEFAULT_ZONEHASH: &str =
     "0x0000000000000000000000000000000000000000000000000000000000000000";
 pub const SEATS_NFT_ADDRESS: &str = "0xD539e71371414F027Af025fd1EfFb6e11b5C902A";
